3ecd18f1d9e74e7c24c666f5f2657537ecc460ea
Péter Szrnka <szrnka.peter@gmail.com>
2023-01-02 14:24:48 +0100

    GMS-3 KeystoreServiceImplTest fixed

--- code/gms-backend/src/test/java/io/github/gms/secure/service/impl/KeystoreServiceImplTest.java
+++ code/gms-backend/src/test/java/io/github/gms/secure/service/impl/KeystoreServiceImplTest.java
@@ -3,6 +3,7 @@ package io.github.gms.secure.service.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -242,7 +243,7 @@ class KeystoreServiceImplTest extends AbstractLoggingUnitTest {
 			GmsException exception = assertThrows(GmsException.class, () -> service.save(model, multiPart));
 	
 			// assert
-			assertEquals("java.io.FileNotFoundException: test-output\\6\\my-key2.jks (A rendszer nem találja a megadott elérési utat)", exception.getMessage());
+			assertTrue(exception.getMessage().startsWith("java.io.FileNotFoundException"));
 			verify(converter).toNewEntity(any(), eq(multiPart));
 			verify(cryptoService).validateKeyStoreFile(any(SaveKeystoreRequestDto.class), any(byte[].class));
 			verify(repository).save(any());
