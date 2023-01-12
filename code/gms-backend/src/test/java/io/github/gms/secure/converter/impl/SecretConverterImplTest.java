fc5daeaf62646ad52f40a9a52ef50d4018f9c7cc
Péter Szrnka <szrnka.peter@gmail.com>
2023-01-12 14:34:59 +0100

    Code quality enhancements

--- code/gms-backend/src/test/java/io/github/gms/secure/converter/impl/SecretConverterImplTest.java
+++ code/gms-backend/src/test/java/io/github/gms/secure/converter/impl/SecretConverterImplTest.java
@@ -1,7 +1,11 @@
 package io.github.gms.secure.converter.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,7 +18,9 @@ import io.github.gms.abstraction.AbstractUnitTest;
 import io.github.gms.common.enums.EntityStatus;
 import io.github.gms.common.enums.RotationPeriod;
 import io.github.gms.secure.dto.SaveSecretRequestDto;
+import io.github.gms.secure.dto.SecretDto;
 import io.github.gms.secure.dto.SecretListDto;
+import io.github.gms.secure.entity.ApiKeyRestrictionEntity;
 import io.github.gms.secure.entity.SecretEntity;
 import io.github.gms.util.TestUtils;
 
@@ -89,5 +95,21 @@ class SecretConverterImplTest extends AbstractUnitTest {
 		// assert
 		assertNotNull(resultList);
 		assertEquals(1, resultList.getResultList().size());
+	}
+	
+	@Test
+	void checkToDto() {
+		// arrange
+		SecretEntity entity = TestUtils.createSecretEntity();
+		ApiKeyRestrictionEntity apiKeyRestrictionEntity1 = new ApiKeyRestrictionEntity();
+		apiKeyRestrictionEntity1.setApiKeyId(1L);
+		
+		// act
+		SecretDto response = converter.toDto(entity, List.of(apiKeyRestrictionEntity1));
+		
+		// assert
+		assertNotNull(response);
+		assertFalse(response.getApiKeyRestrictions().isEmpty());
+		assertTrue(response.getApiKeyRestrictions().contains(1L));
 	}
 }
