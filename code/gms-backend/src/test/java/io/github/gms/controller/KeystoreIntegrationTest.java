c5f1bc233a466a0a86e036aeae748222f751f9f6
Péter Szrnka <szrnka.peter@gmail.com>
2023-02-16 13:33:02 +0100

    GMS-31 New integration test created for the new function

--- code/gms-backend/src/test/java/io/github/gms/controller/KeystoreIntegrationTest.java
+++ code/gms-backend/src/test/java/io/github/gms/controller/KeystoreIntegrationTest.java
@@ -17,7 +17,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +39,7 @@ import io.github.gms.abstraction.AbstractClientControllerIntegrationTest;
 import io.github.gms.common.enums.EntityStatus;
 import io.github.gms.common.enums.KeyStoreValueType;
 import io.github.gms.common.filter.SecureHeaderInitializerFilter;
+import io.github.gms.common.util.Constants;
 import io.github.gms.secure.controller.KeystoreController;
 import io.github.gms.secure.dto.GetSecureValueDto;
 import io.github.gms.secure.dto.IdNamePairListDto;
@@ -57,13 +60,13 @@ import lombok.SneakyThrows;
  */
 @Tag(TestConstants.TAG_INTEGRATION_TEST)
 class KeystoreIntegrationTest extends AbstractClientControllerIntegrationTest {
-	
+
 	@Autowired
 	private SecureHeaderInitializerFilter secureHeaderInitializerFilter;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
@@ -76,48 +79,44 @@ class KeystoreIntegrationTest extends AbstractClientControllerIntegrationTest {
 	void testSave() {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream jksFileStream = classloader.getResourceAsStream("test.jks");
-		
-	    MockMultipartFile sampleFile = new MockMultipartFile(
-	      KeystoreController.MULTIPART_FILE,
-	      "test-" + UUID.randomUUID().toString() + ".jks", 
-	      MediaType.APPLICATION_OCTET_STREAM_VALUE,
-	      jksFileStream.readAllBytes()
-	    );
-	    
-	    String saveRequestJson = objectMapper.writeValueAsString(TestUtils.createSaveKeystoreRequestDto());
-	 
-	    MockMultipartHttpServletRequestBuilder multipartRequest =
-	      MockMvcRequestBuilders.multipart("/secure/keystore")
-	      .file(sampleFile);
-	    
-	    multipartRequest.flashAttr("model", saveRequestJson);
-	    
-	    multipartRequest.headers(TestUtils.getHttpHeaders(null));
-	    multipartRequest.cookie(TestUtils.getCookie(jwt));
 
-	    MockMvc mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-	    		.addFilter(secureHeaderInitializerFilter, "/secure/keystore")
-	    		.build();
-	 
-	    // act
-	    mvc.perform(multipartRequest).andExpect(MockMvcResultMatchers.status().isOk());
+		MockMultipartFile sampleFile = new MockMultipartFile(KeystoreController.MULTIPART_FILE,
+				"test-" + UUID.randomUUID().toString() + ".jks", MediaType.APPLICATION_OCTET_STREAM_VALUE,
+				jksFileStream.readAllBytes());
+
+		String saveRequestJson = objectMapper.writeValueAsString(TestUtils.createSaveKeystoreRequestDto());
+
+		MockMultipartHttpServletRequestBuilder multipartRequest = MockMvcRequestBuilders.multipart("/secure/keystore")
+				.file(sampleFile);
+
+		multipartRequest.flashAttr("model", saveRequestJson);
+
+		multipartRequest.headers(TestUtils.getHttpHeaders(null));
+		multipartRequest.cookie(TestUtils.getCookie(jwt));
+
+		MockMvc mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.addFilter(secureHeaderInitializerFilter, "/secure/keystore").build();
+
+		// act
+		mvc.perform(multipartRequest).andExpect(MockMvcResultMatchers.status().isOk());
 	}
-	
+
 	@Test
 	void testGetById() {
 		// act
 		HttpEntity<Void> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
-		ResponseEntity<KeystoreDto> response = executeHttpGet("/" + DemoData.KEYSTORE_ID, requestEntity, KeystoreDto.class);
+		ResponseEntity<KeystoreDto> response = executeHttpGet("/" + DemoData.KEYSTORE_ID, requestEntity,
+				KeystoreDto.class);
 
 		// Assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
-		
+
 		KeystoreDto responseBody = response.getBody();
 		assertEquals(DemoData.KEYSTORE_ID, responseBody.getId());
 		assertEquals(EntityStatus.ACTIVE, responseBody.getStatus());
 	}
-	
+
 	@Test
 	void testList() {
 		// act
@@ -129,11 +128,11 @@ class KeystoreIntegrationTest extends AbstractClientControllerIntegrationTest {
 		// Assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
-		
+
 		KeystoreListDto responseList = response.getBody();
 		assertEquals(2, responseList.getResultList().size());
 	}
-	
+
 	@ParameterizedTest
 	@MethodSource("valueData")
 	void testGetSecretValue(ValueHolder input) {
@@ -145,94 +144,128 @@ class KeystoreIntegrationTest extends AbstractClientControllerIntegrationTest {
 		// Assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
-		
+
 		String responseBody = response.getBody();
 		assertEquals(input.getExpectedValue(), responseBody);
 	}
-	
+
 	@Test
 	@SneakyThrows
 	void testDelete() {
 		// arrange
 		KeystoreEntity newEntity = keystoreRepository.save(TestUtils.createNewKeystoreEntity(3L));
-		
+
 		// act
 		HttpEntity<Void> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
-		ResponseEntity<String> response = executeHttpDelete("/" + newEntity.getId(), requestEntity,
-				String.class);
+		ResponseEntity<String> response = executeHttpDelete("/" + newEntity.getId(), requestEntity, String.class);
 
 		// Assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNull(response.getBody());
-		
+
 		Files.copy(new File("src/test/resources/test.jks"), new File("src/test/resources/1/test.jks"));
 	}
-	
+
 	@Transactional
 	@ParameterizedTest
 	@ValueSource(booleans = { false, true })
 	void testToggleStatus(boolean enabled) {
 		// act
 		HttpEntity<Void> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
-		ResponseEntity<String> response = executeHttpPost("/" + DemoData.KEYSTORE_ID + "?enabled="+ enabled, requestEntity,
-				String.class);
+		ResponseEntity<String> response = executeHttpPost("/" + DemoData.KEYSTORE_ID + "?enabled=" + enabled,
+				requestEntity, String.class);
 
 		// assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNull(response.getBody());
-		
+
 		KeystoreEntity entity = keystoreRepository.getReferenceById(DemoData.KEYSTORE_ID);
 		assertNotNull(entity);
 		assertEquals(enabled ? EntityStatus.ACTIVE : EntityStatus.DISABLED, entity.getStatus());
-		
-		executeHttpPost("/" + DemoData.KEYSTORE_ID + "?enabled="+ true, requestEntity,String.class);
+
+		executeHttpPost("/" + DemoData.KEYSTORE_ID + "?enabled=" + true, requestEntity, String.class);
 	}
-	
+
 	@Test
 	void testCount() {
+		// arrange
 		HttpEntity<GetSecureValueDto> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
+
 		// act
 		ResponseEntity<LongValueDto> response = executeHttpGet("/count", requestEntity, LongValueDto.class);
-		
+
 		// assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
 		assertEquals(2L, response.getBody().getValue());
 	}
-	
+
 	@Test
 	void testGetAllKeystoreNames() {
+		// arrange
 		HttpEntity<GetSecureValueDto> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
-		
+
 		// act
-		ResponseEntity<IdNamePairListDto> response = executeHttpGet("/list_names", requestEntity, IdNamePairListDto.class);
-		
+		ResponseEntity<IdNamePairListDto> response = executeHttpGet("/list_names", requestEntity,
+				IdNamePairListDto.class);
+
 		// assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
 		assertFalse(response.getBody().getResultList().isEmpty());
 		assertEquals(2, response.getBody().getResultList().size());
 	}
-	
+
 	@Test
 	void testGetAllKeystoreAliasNames() {
-		HttpEntity<GetSecureValueDto> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
-		
+		// arrange
+		HttpEntity<Void> requestEntity = new HttpEntity<>(TestUtils.getHttpHeaders(jwt));
+
 		// act
-		ResponseEntity<IdNamePairListDto> response = executeHttpGet("/list_aliases/" + DemoData.KEYSTORE_ID, requestEntity, IdNamePairListDto.class);
-		
+		ResponseEntity<IdNamePairListDto> response = executeHttpGet("/list_aliases/" + DemoData.KEYSTORE_ID,
+				requestEntity, IdNamePairListDto.class);
+
 		// assert
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
 		assertFalse(response.getBody().getResultList().isEmpty());
 		assertEquals(2, response.getBody().getResultList().size());
 	}
-	
+
+	@Test
+	@SneakyThrows
+	void testDownloadKeystoreFile() {
+		// arrange
+		File keystoreFile = new File("./keystores/1/test.jks");
+		boolean keystoreFileCreated = !keystoreFile.exists();
+		
+		if (!keystoreFile.exists()) {
+			Files.createParentDirs(keystoreFile);
+			keystoreFile.createNewFile();
+			Files.write("test".getBytes(), keystoreFile);
+		}
+		
+		HttpHeaders headers = new HttpHeaders();	
+		headers.add("Cookie", Constants.ACCESS_JWT_TOKEN + "=" + jwt + ";Max-Age=3600;HttpOnly");
+		
+		HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+		// act
+		ResponseEntity<Resource> response = executeHttpGet("/download/" + DemoData.KEYSTORE_ID, requestEntity,
+				Resource.class);
+
+		// assert
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		
+		if (keystoreFileCreated) {
+			keystoreFile.delete();
+		}
+	}
+
 	public static List<ValueHolder> valueData() {
-		return Lists.newArrayList(
-				new ValueHolder(KeyStoreValueType.KEYSTORE_ALIAS, DemoData.KEYSTORE_ALIAS_ID, "test"),
+		return Lists.newArrayList(new ValueHolder(KeyStoreValueType.KEYSTORE_ALIAS, DemoData.KEYSTORE_ALIAS_ID, "test"),
 				new ValueHolder(KeyStoreValueType.KEYSTORE_ALIAS_CREDENTIAL, DemoData.KEYSTORE_ALIAS_ID, "test"),
-				new ValueHolder(KeyStoreValueType.KEYSTORE_CREDENTIAL, null, "test")
-		);
+				new ValueHolder(KeyStoreValueType.KEYSTORE_CREDENTIAL, null, "test"));
 	}
 }
