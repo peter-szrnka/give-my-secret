b6d82bc599d9129a8df671fd5b86d1bf35782d6c
Péter Szrnka <szrnka.peter@gmail.com>
2023-02-16 08:21:26 +0100

    Test commit

--- code/gms-frontend/webpack.config.js
+++ code/gms-frontend/webpack.config.js
@@ -1,5 +1,22 @@
 /* eslint-disable no-undef */
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loader: 'postcss-loader',
+                options: {
+                    ident: 'postcss',
+                    syntax: 'postcss-scss',
+                    plugins: () => [
+                        require('postcss-import'),
+                        require('tailwindcss'),
+                        require('autoprefixer'),
+                    ]
+                }
+            }
+        ]
+    },
     output: {
         hashFunction: "xxhash64"
     }
