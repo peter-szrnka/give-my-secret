8e45258a8b233a361dec696e6b089845cc236fb0
Péter Szrnka <szrnka.peter@gmail.com>
2023-09-07 20:11:51 +0200

    Remaining test fix attempt 1

--- code/gms-backend/src/test/java/io/github/gms/GmsExceptionHandlerTest.java
+++ code/gms-backend/src/test/java/io/github/gms/GmsExceptionHandlerTest.java
@@ -3,13 +3,15 @@ package io.github.gms;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
 
 import org.jboss.logging.MDC;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.access.AccessDeniedException;
@@ -26,7 +28,6 @@ import jakarta.servlet.http.HttpServletRequest;
  * @author Peter Szrnka
  * @since 1.0
  */
-@Disabled
 class GmsExceptionHandlerTest extends AbstractUnitTest {
 
 	private static final String CORRELATION_ID = "CORRELATION_ID";
@@ -36,7 +37,9 @@ class GmsExceptionHandlerTest extends AbstractUnitTest {
 	@BeforeEach
 	public void setup() {
 		MDC.put(MdcParameter.CORRELATION_ID.getDisplayName(), CORRELATION_ID);
-		Clock clock = Clock.systemDefaultZone();
+		Clock clock = mock(Clock.class);
+		when(clock.instant()).thenReturn(Instant.parse("2023-06-29T00:00:00Z"));
+		when(clock.getZone()).thenReturn(ZoneOffset.UTC);
 		handler = new GmsExceptionHandler(clock);
 	}
 
