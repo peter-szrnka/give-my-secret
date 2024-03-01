43e8bc23d826815884816195e95871f0e5c11e5a
Péter Szrnka <szrnka.peter@gmail.com>
2024-03-01 09:44:29 +0100

    GMS-101 Simplifications, deletions of unused code parts, config changes

--- code/gms-backend/src/main/java/io/github/gms/common/config/ApplicationConfig.java
+++ code/gms-backend/src/main/java/io/github/gms/common/config/ApplicationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -35,8 +36,8 @@ public class ApplicationConfig implements WebMvcConfigurer {
 	@Bean("secretRotationExecutor")
     public TaskExecutor getAsyncExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);
-        executor.setMaxPoolSize(4);
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setThreadNamePrefix("secret-rotation-");
         return executor;
@@ -67,7 +68,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                    protected Resource getResource(@NonNull String resourcePath, @NonNull Resource location) throws IOException {
                         Resource requestedResource = location.createRelative(resourcePath);
 
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
@@ -75,16 +76,4 @@ public class ApplicationConfig implements WebMvcConfigurer {
                     }
                 });
     }
-
-	/*@Bean
-    public TomcatServletWebServerFactory containerFactory() {
-        return new TomcatServletWebServerFactory() {
-            @Override
-            protected void customizeConnector(Connector connector) {
-                super.customizeConnector(connector);
-                
-                addContextCustomizers(context -> context.setCookieProcessor(new LegacyCookieProcessor()));
-            }
-        };
-    }*/
 }
