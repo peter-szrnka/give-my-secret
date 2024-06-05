df36fa6346bb26bc275c4bad303124257c83b3f2
Péter Szrnka <szrnka.peter@gmail.com>
2024-06-05 12:45:11 +0200

    GMS-123 New functions added to LoggerService

--- code/gms-frontend/src/app/common/service/logger-service.spec.ts
+++ code/gms-frontend/src/app/common/service/logger-service.spec.ts
@@ -6,7 +6,17 @@ import { LoggerService } from "./logger-service";
 describe('LoggerService', () => {
     let service: LoggerService = new LoggerService();
 
-    it('should print info log message', () => {
+    it('should print error message', () => {
+        console.error = jest.fn();
+
+        // act
+        service.error('message', 'param');
+
+        // assert
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should print info message', () => {
         console.info = jest.fn();
 
         // act
@@ -14,5 +24,25 @@ describe('LoggerService', () => {
 
         // assert
         expect(console.info).toHaveBeenCalled();
+    });
+
+    it('should print log message', () => {
+        console.log = jest.fn();
+
+        // act
+        service.log('message', 'param');
+
+        // assert
+        expect(console.log).toHaveBeenCalled();
+    });
+
+    it('should print warn message', () => {
+        console.warn = jest.fn();
+
+        // act
+        service.warn('message', 'param');
+
+        // assert
+        expect(console.warn).toHaveBeenCalled();
     });
 });
--- code/gms-frontend/src/app/common/service/logger-service.ts
+++ code/gms-frontend/src/app/common/service/logger-service.ts
@@ -6,7 +6,19 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class LoggerService {
 
+    public error(message: string, ...optionalParams: any): void {
+        console.error(message, optionalParams);
+    }
+
     public info(message: string, ...optionalParams: any): void {
         console.info(message, optionalParams);
+    }
+
+    public log(message: string, ...optionalParams: any): void {
+        console.log(message, optionalParams);
+    }
+
+    public warn(message: string, ...optionalParams: any): void {
+        console.warn(message, optionalParams);
     }
 }
