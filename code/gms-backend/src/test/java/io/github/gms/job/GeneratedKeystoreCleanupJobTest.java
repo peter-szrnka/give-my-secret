8e3ce3ad4d9f2e34899644d9f884a428796f2770
Péter Szrnka <szrnka.peter@gmail.com>
2024-07-31 16:52:09 +0200

    GMS-125 Fix of failed tests

--- code/gms-backend/src/test/java/io/github/gms/job/GeneratedKeystoreCleanupJobTest.java
+++ code/gms-backend/src/test/java/io/github/gms/job/GeneratedKeystoreCleanupJobTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
--- code/gms-backend/src/test/java/io/github/gms/job/MessageCleanupJobTest.java
+++ code/gms-backend/src/test/java/io/github/gms/job/MessageCleanupJobTest.java
@@ -18,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -59,7 +58,7 @@ class MessageCleanupJobTest extends AbstractLoggingUnitTest {
 		// assert
 		assertTrue(logAppender.list.isEmpty());
 		verify(env).getProperty("HOSTNAME");
-		verify(systemPropertyService, times(2)).get(SystemProperty.MESSAGE_CLEANUP_RUNNER_CONTAINER_ID);
+		verify(systemPropertyService).get(SystemProperty.MESSAGE_CLEANUP_RUNNER_CONTAINER_ID);
 	}
 	
 	@Test
