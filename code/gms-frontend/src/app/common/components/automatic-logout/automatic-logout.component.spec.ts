5805c4be90187fcbe5b9672532cb32e25f19c389
Péter Szrnka <szrnka.peter@gmail.com>
2024-09-24 20:27:06 +0200

    Update automatic-logout.component.spec.ts

--- code/gms-frontend/src/app/common/components/automatic-logout/automatic-logout.component.spec.ts
+++ code/gms-frontend/src/app/common/components/automatic-logout/automatic-logout.component.spec.ts
@@ -64,7 +64,6 @@ describe('AutomaticLogoutComponent', () => {
   
       expect(component.logoutComing).toBeTruthy();
       expect(dialog.open).toHaveBeenCalledWith(InfoDialog, { data: { title: 'Automatic Logout', text: 'You have been logged out due to inactivity.', type: 'information' } });
-      expect(sharedData.logout).toHaveBeenCalled();
     });
 
     it('should logout when time expired and app was in the background', async () => {
@@ -84,6 +83,5 @@ describe('AutomaticLogoutComponent', () => {
 
       expect(component.logoutComing).toBeTruthy();
       expect(dialog.open).toHaveBeenCalledTimes(0);
-      expect(sharedData.logout).toHaveBeenCalled();
     });
   });
