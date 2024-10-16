79e790ce870a61a103c2222aee2108f6cf66f191
Péter Szrnka <szrnka.peter@gmail.com>
2024-10-16 16:42:54 +0200

    GMS-0 Version reverted to 1.0.6-SNAPSHOT + Checkstyle failure fixed

--- code/gms-backend/src/main/java/io/github/gms/common/model/UserMaintenanceConfig.java
+++ code/gms-backend/src/main/java/io/github/gms/common/model/UserMaintenanceConfig.java
@@ -5,6 +5,9 @@ import io.github.gms.common.enums.EntityStatus;
 /**
  * @author Peter Szrnka
  * @since 1.0
+ *
+ * @param scope Service scope
+ * @param status Expected user status
  */
 public record UserMaintenanceConfig(String scope, EntityStatus status) {
 }
