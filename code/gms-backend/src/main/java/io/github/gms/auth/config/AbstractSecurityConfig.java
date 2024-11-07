10ba010a59d63c83f92e0f65564a1d5dba963079
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-07 13:22:15 +0100

    GMS-158 Security config updated

--- code/gms-backend/src/main/java/io/github/gms/auth/config/AbstractSecurityConfig.java
+++ code/gms-backend/src/main/java/io/github/gms/auth/config/AbstractSecurityConfig.java
@@ -28,12 +28,12 @@ import java.util.List;
 @Slf4j
 public abstract class AbstractSecurityConfig {
 
-    private static final String[] FILTER_URL = new String[]{"/", "/system/status", "/healthcheck",
+    private static final String[] FILTER_URL = new String[]{"/", "/info/status", "/healthcheck",
             "/setup/vm_options", "/setup/step_back", "/setup/initial", "/setup/user",
             "/setup/properties", "/setup/org_data", "/setup/current_super_admin", "/setup/complete",
             "/login", "/authenticate", "/verify", "/logoutUser", "/api/**", "/info/me", "/actuator/**",
             "/gms-app/**", "/favicon.ico", "/assets/**", "/index.html**", "/*.js**", "/*.css**", "/*.json**",
-            "/manifest.webmanifest", "/reset_password", "/error_codes"};
+            "/manifest.webmanifest", "/reset_password", "/info/error_codes"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
