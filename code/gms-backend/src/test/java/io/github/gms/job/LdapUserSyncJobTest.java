725dee1214a38c9081b9d83834bce7108f0c5927
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-10 21:52:04 +0100

    GMS-164 Fixed unit tests

--- code/gms-backend/src/test/java/io/github/gms/job/LdapUserSyncJobTest.java
+++ code/gms-backend/src/test/java/io/github/gms/job/LdapUserSyncJobTest.java
@@ -3,10 +3,13 @@ package io.github.gms.job;
 import io.github.gms.abstraction.AbstractLoggingUnitTest;
 import io.github.gms.auth.ldap.LdapSyncService;
 import io.github.gms.common.enums.SystemProperty;
+import io.github.gms.common.enums.SystemStatus;
 import io.github.gms.functions.maintenance.job.JobEntity;
 import io.github.gms.functions.maintenance.job.JobRepository;
+import io.github.gms.functions.setup.SystemAttributeRepository;
 import io.github.gms.functions.system.SystemService;
 import io.github.gms.functions.systemproperty.SystemPropertyService;
+import io.github.gms.util.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,6 +20,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 import static io.github.gms.util.LogAssertionUtils.assertLogContains;
 import static io.github.gms.util.TestUtils.createJobEntity;
@@ -35,6 +39,7 @@ class LdapUserSyncJobTest extends AbstractLoggingUnitTest {
     private LdapSyncService service;
     private Clock clock;
     private JobRepository jobRepository;
+    private SystemAttributeRepository systemAttributeRepository;
     private LdapUserSyncJob job;
 
     @Override
@@ -48,20 +53,36 @@ class LdapUserSyncJobTest extends AbstractLoggingUnitTest {
         service = mock(LdapSyncService.class);
         clock = mock(Clock.class);
         jobRepository = mock(JobRepository.class);
+        systemAttributeRepository = mock(SystemAttributeRepository.class);
         job = new LdapUserSyncJob(service);
 
         ReflectionTestUtils.setField(job, "systemService", systemService);
         ReflectionTestUtils.setField(job, "systemPropertyService", systemPropertyService);
         ReflectionTestUtils.setField(job, "clock", clock);
         ReflectionTestUtils.setField(job, "jobRepository", jobRepository);
+        ReflectionTestUtils.setField(job, "systemAttributeRepository", systemAttributeRepository);
 
         addAppender(LdapUserSyncJob.class);
+    }
+
+    @Test
+    void run_whenSystemIsNotReady_thenSkipExecution() {
+        // arrange
+        when(systemAttributeRepository.getSystemStatus()).thenReturn(Optional.of(TestUtils.createSystemAttributeEntity(SystemStatus.NEED_SETUP)));
+
+        // act
+        job.run();
+
+        // assert
+        assertTrue(logAppender.list.isEmpty());
+        verify(systemAttributeRepository).getSystemStatus();
     }
 
     @Test
     void run_whenJobIsDisabled_thenSkipExecution() {
         // arrange
         when(systemPropertyService.getBoolean(SystemProperty.LDAP_SYNC_JOB_ENABLED)).thenReturn(false);
+        when(systemAttributeRepository.getSystemStatus()).thenReturn(Optional.of(TestUtils.createSystemAttributeEntity(SystemStatus.OK)));
 
         // act
         job.run();
@@ -78,6 +99,7 @@ class LdapUserSyncJobTest extends AbstractLoggingUnitTest {
         when(systemPropertyService.getBoolean(SystemProperty.LDAP_SYNC_JOB_ENABLED)).thenReturn(true);
         when(systemPropertyService.getBoolean(SystemProperty.ENABLE_MULTI_NODE)).thenReturn(true);
         when(systemPropertyService.get(SystemProperty.LDAP_SYNC_RUNNER_CONTAINER_ID)).thenReturn("ab123456");
+        when(systemAttributeRepository.getSystemStatus()).thenReturn(Optional.of(TestUtils.createSystemAttributeEntity(SystemStatus.OK)));
 
         // act
         job.run();
@@ -98,6 +120,7 @@ class LdapUserSyncJobTest extends AbstractLoggingUnitTest {
         when(jobRepository.findById(anyLong())).thenReturn(java.util.Optional.of(createJobEntity()));
         when(clock.instant()).thenReturn(Instant.parse("2023-06-29T00:00:00Z"));
         when(clock.getZone()).thenReturn(ZoneOffset.UTC);
+        when(systemAttributeRepository.getSystemStatus()).thenReturn(Optional.of(TestUtils.createSystemAttributeEntity(SystemStatus.OK)));
 
         // act
         job.run();
