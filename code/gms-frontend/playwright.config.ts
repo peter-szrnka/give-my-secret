e1ddd192c5cc1dcfe63c2bb4cbc1140886d7b777
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-19 09:14:42 +0100

    GMS-167 PlayWright config updated

--- code/gms-frontend/playwright.config.ts
+++ code/gms-frontend/playwright.config.ts
@@ -16,11 +16,11 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: false,/* !!process.env.CI,*/
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1, /*process.env.CI ? 2 : 0,*/
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: undefined,/*process.env.CI ? 1 : undefined,*/
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
