02cc1585aac1d9695a8378692d9c97712ad9af32
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-19 10:42:30 +0100

    GMS-167 Component test fixed

--- code/gms-frontend/src/ui-test/about.component.spec.ts
+++ code/gms-frontend/src/ui-test/about.component.spec.ts
@@ -1,15 +1,15 @@
 import { expect, test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(`/about`);
-});
-
 test.describe('About component', () => {
 
-    test('should load main texts', async ({ page }) => {
-      await expect(page.getByText('About Give My Secret')).toBeVisible();
-      await expect(page.locator('li').getByText('Application version: MOCK')).toBeVisible();
-      await expect(page.locator('li').getByText('Built time')).toBeVisible();
-      await expect(page.locator('li').getByText('Status: OK')).toBeVisible();
-    });
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/about`);
+  });
+
+  test('should load main texts', async ({ page }) => {
+    await expect(page.getByText('About Give My Secret')).toBeVisible();
+    await expect(page.locator('li').getByText('Application version: MOCK')).toBeVisible();
+    await expect(page.locator('li').getByText('Built time')).toBeVisible();
+    await expect(page.locator('li').getByText('Status: OK')).toBeVisible();
+  });
 });
