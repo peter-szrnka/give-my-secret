3f1c85d6b42a09d1c37d366c3001e58ee4ca94d4
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-22 15:43:10 +0100

    GMS-168 Code smell fixed

--- code/gms-frontend/src/app/components/system_property/system-property-list.component.ts
+++ code/gms-frontend/src/app/components/system_property/system-property-list.component.ts
@@ -179,8 +179,12 @@ export class SystemPropertyListComponent {
     this.fetchData();
   }
 
+  private convertBackToString(element: SystemProperty): string {
+    return element.value ? 'true' : 'false';
+  }
+
   public save(element: SystemProperty) {
-    element.value = "BOOLEAN" === element.type ? element.value ? 'true' : 'false' : element.value;
+    element.value = "BOOLEAN" === element.type ? this.convertBackToString(element) : element.value;
     element.mode = undefined;
     element.valueSet = undefined;
     this.service.save(element).subscribe({
