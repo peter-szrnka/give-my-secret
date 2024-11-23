749600d324c3da25287f9d44e20a8016f654b758
Péter Szrnka <szrnka.peter@gmail.com>
2024-11-23 15:10:42 +0100

    GMS-169 PlayWright config fix

--- code/gms-frontend/playwright.config.ts
+++ code/gms-frontend/playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './src/ui-test',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: !!process.env.CI,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
