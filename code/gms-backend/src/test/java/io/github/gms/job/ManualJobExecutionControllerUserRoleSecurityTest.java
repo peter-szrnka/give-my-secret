65f397ad3fb1ab32405e277c34eaa1be230e3e76
Péter Szrnka <szrnka.peter@gmail.com>
2024-12-04 19:07:27 +0100

    GMS-173 Missing annotations added to security test

--- code/gms-backend/src/test/java/io/github/gms/job/ManualJobExecutionControllerUserRoleSecurityTest.java
+++ code/gms-backend/src/test/java/io/github/gms/job/ManualJobExecutionControllerUserRoleSecurityTest.java
@@ -2,6 +2,7 @@ package io.github.gms.job;
 
 import io.github.gms.abstraction.AbstractUserRoleSecurityTest;
 import io.github.gms.common.TestedClass;
+import io.github.gms.common.TestedMethod;
 import io.github.gms.util.TestUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -26,41 +27,49 @@ class ManualJobExecutionControllerUserRoleSecurityTest extends AbstractUserRoleS
     }
 
     @Test
+    @TestedMethod("eventMaintenance")
     void eventMaintenance_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(EVENT_MAINTENANCE);
     }
 
     @Test
+    @TestedMethod("generatedKeystoreCleanup")
     void generatedKeystoreCleanup_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(GENERATED_KEYSTORE_CLEANUP);
     }
 
     @Test
+    @TestedMethod("jobMaintenance")
     void jobMaintenance_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(JOB_MAINTENANCE);
     }
 
     @Test
+    @TestedMethod("messageCleanup")
     void messageCleanup_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(MESSAGE_CLEANUP);
     }
 
     @Test
+    @TestedMethod("secretRotation")
     void secretRotation_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(SECRET_ROTATION);
     }
 
     @Test
+    @TestedMethod("userAnonymization")
     void userAnonymization_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(USER_ANONYMIZATION);
     }
 
     @Test
+    @TestedMethod("userDeletion")
     void userDeletion_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(USER_DELETION);
     }
 
     @Test
+    @TestedMethod("ldapUserSync")
     void ldapUserSync_whenUserIsNull_thenReturnHttp403() {
         assertByUrl(LDAP_USER_SYNC);
     }
