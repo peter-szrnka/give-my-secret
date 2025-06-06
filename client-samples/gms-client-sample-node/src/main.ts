2a1d80a7cb8e1b733e08a9ed8ba5aa30e9e89670
Péter Szrnka <szrnka.peter@gmail.com>
2025-06-06 10:17:11 +0200

    GMS-0 Sample fixed with a temp solution + NestJs + Jest removed

--- client-samples/gms-client-sample-node/src/main.ts
+++ client-samples/gms-client-sample-node/src/main.ts
@@ -1,10 +1,42 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import fs from 'fs';
+import jks from 'jks-js';
+import crypto from 'crypto';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const BASE_URL : string = "https://localhost:8443/api/secret/";
+// Your variables //////////////////////////////////////////////////
+const SECRET_ID1 : string = "secret1";
+const SECRET_ID2 : string = "secret2";
+const API_KEY : string = "xBpFOEEjfZWpSXSuOXpGoGt3PVvuGTYq";
+////////////////////////////////////////////////////////////////////
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const fetchApi = (secretId: string, filename: string) => {
+    fetch(`${BASE_URL}${secretId}`, { method: 'GET', headers: { "Content-Type": "application/json",'x-api-key': API_KEY }})
+        .then(async (resp: any) => {
+          const response = await resp;
+          if (response.status !== 200) {
+            throw new Error('Invalid response!');
+          }
+
+          const value = (await response.json())['value'];
+          let decryptedData : Buffer = decryptData(value, filename);
+          console.info("Finally decrypted value is = ", decryptedData.toString('utf8'));
+        });
+};
+
+const decryptData = (value : string, filename : string) : Buffer => {
+    const keystore = jks.toPem(fs.readFileSync(filename), 'test');
+    const key = keystore['test'].key;
+    const decryptedData = crypto.privateDecrypt(
+      {
+        key: key,
+        passphrase: 'test',
+        padding: crypto.constants.RSA_PKCS1_PADDING
+      },
+      Buffer.from(value, "base64"),
+    );
+
+    return decryptedData;
+};
+
+fetchApi(SECRET_ID1, 'test.jks');
+fetchApi(SECRET_ID2, 'test.p12');
