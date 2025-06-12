9191f42b28224a3b7d26651e060bec8ae45d8f99
Péter Szrnka <szrnka.peter@gmail.com>
2025-06-12 07:20:11 +0200

    GMS-0 Fix of window injection token

--- code/gms-frontend/src/main.ts
+++ code/gms-frontend/src/main.ts
@@ -1,16 +1,17 @@
 import { enableProdMode } from '@angular/core';
 
-import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withDebugTracing } from '@angular/router';
-import { routes, ENV_CONFIG } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { ENV_CONFIG, routes } from './app/app.config';
 import { AuthInterceptor } from './app/common/interceptor/auth-interceptor';
 import { CsrfTokenInterceptor } from './app/common/interceptor/csrf-token-interceptor';
 import { MockInterceptor } from './app/common/interceptor/mock-empty-interceptor';
+import { WINDOW_TOKEN } from './app/window.provider';
+import { environment } from './environments/environment';
 
 /**
  * @author Peter Szrnka
@@ -21,13 +22,13 @@ const APP_CONFIG = {
       ...(environment.production ? [] : [withDebugTracing()])
     ),
     { provide: ENV_CONFIG, useValue: environment },
+    { provide: WINDOW_TOKEN, useFactory: () => (typeof window !== 'undefined' ? window : null) },
     { provide: HTTP_INTERCEPTORS, useClass: CsrfTokenInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: MockInterceptor, multi: true },
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations(),
-    provideClientHydration()
+    provideAnimations()
   ]
 };
 
