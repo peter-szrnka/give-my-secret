b0872b9b428babfe851e5f13ba4e823ec726fa77
Péter Szrnka <szrnka.peter@gmail.com>
2026-03-01 20:11:56 +0100

    GMS-202 Fix attempt

--- code/gms-frontend/src/test-setup.ts
+++ code/gms-frontend/src/test-setup.ts
@@ -2,3 +2,9 @@
 import 'zone.js';
 import 'zone.js/testing';
 import '@angular/compiler';
+import "cross-fetch/polyfill";
+import { vi } from "vitest";
+
+global.fetch = global.fetch ?? vi.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve({}) })
+);
